5c3e66d1a6baa5a5a1a831ae1973f7fbae0deff4
Cristal Flores <cristalflores.bo@gmail.com>
2024-05-11 01:36:41 -0400

    component naming best practices

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,6 +1,6 @@
 // Define a local component
-let PlanComponent = {
-    template: '#plan-template',
+let PlanPickerItemComponent = {
+    template: '#plan-picker-item-template',
     props: {
         name: {
             type: String,
@@ -26,7 +26,7 @@ let PlanComponent = {
 let PlanPickerComponent = {
     template: '#plan-picker-template',
     components: {
-        plan: PlanComponent
+        'plan-picker-item': PlanPickerItemComponent
     },
     data() {
         return {
