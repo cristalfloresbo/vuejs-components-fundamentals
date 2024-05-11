729a3a1c84d239dbfb6c2e9777b2eddfd636ee26
Cristal Flores <cristalflores.bo@gmail.com>
2024-05-10 20:22:24 -0400

    global vs local components

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,13 +1,5 @@
-Vue.component('plan-picker', {
-    template: '#plan-picker-template',
-    data() {
-        return {
-            plans: ['The Single', 'The Curious', 'The Addict']
-        }
-    }
-})
-
-Vue.component('plan', {
+// Define a local component
+let PlanComponent = {
     template: '#plan-template',
     props: {
         name: {
@@ -15,8 +7,25 @@ Vue.component('plan', {
             required: true
         }
     }
-})
+}
 
+// Create a parent Vue component
+let PlanPickerComponent = {
+    template: '#plan-picker-template',
+    components: {
+        plan: PlanComponent
+    },
+    data() {
+        return {
+            plans: ['The Single', 'The Curious', 'The Addict']
+        }
+    }
+}
+
+// Create a Vue instance
 new Vue({
-    el: '#app'
+    el: '#app',
+    components: {
+        'plan-picker': PlanPickerComponent
+    }
 })
